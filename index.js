42adc275fd13fa2bcb06b6e890e7b1add50a3af7
parksunjin00 <lovesj7595@gmail.com>
2024-12-25 22:39:04 +0900

    .

--- index.js
+++ index.js
@@ -2,27 +2,27 @@
 const $cursor = $('.cursor');
 const $cursorShadow = $('.cursor-shadow');
 
-$(window).mousemove(function(e){
+$(window).mousemove(function (e) {
     console.log("e.clientX : " + e.clientX);
     console.log("e.clientY : " + e.clientY);
 
     $cursor.css({
-        left:e.clientX,
-        top:e.clientY
+        left: e.clientX,
+        top: e.clientY
     });
 
     $cursorShadow.css({
-        left:e.clientX,
-        top:e.clientY
+        left: e.clientX,
+        top: e.clientY
     });
 });
 
 
-$('.cursor-big').mouseenter(function(){
+$('.cursor-big').mouseenter(function () {
     $('html').addClass('need-to-cursor-big');
 });
 
-$('.cursor-big').mouseleave(function(){
+$('.cursor-big').mouseleave(function () {
     $('html').removeClass('need-to-cursor-big');
 });
 
@@ -30,68 +30,137 @@ $('.cursor-big').mouseleave(function(){
 // 스크롤 트리거 플러그인 활성화
 gsap.registerPlugin(ScrollTrigger);
 
-function TopBar__init() {
+function HeaderBar__init() {
     var scrub = true;
 
-    gsap.to('.top-bar__logo > span > span.fast1', {
+    gsap.to('.header-bar__logo > span > span.fast1', {
         yPercent: 30,
         scrollTrigger: {
-            trigger: '.top-bar__logo',
-            markers: true,
+            trigger: '.header-bar__logo',
+            markers: false,
             start: 'top 100px',
             scrub: scrub
         }
     });
 
-    gsap.to('.top-bar__logo > span > span.fast2', {
+    gsap.to('.header-bar__logo > span > span.fast2', {
         yPercent: 20,
         scrollTrigger: {
-            trigger: '.top-bar__logo',
-            markers: true,
+            trigger: '.header-bar__logo',
+            markers: false,
             start: 'top 100px',
             scrub: scrub
         }
     });
 
-    gsap.to('.top-bar__logo > span > span.fast3', {
+    gsap.to('.header-bar__logo > span > span.fast3', {
         yPercent: 10,
         scrollTrigger: {
-            trigger: '.top-bar__logo',
-            markers: true,
+            trigger: '.header-bar__logo',
+            markers: false,
             start: 'top 100px',
             scrub: scrub
         }
     });
 
-    gsap.to('.top-bar__logo > span > span.slow1', {
+    gsap.to('.header-bar__logo > span > span.slow1', {
         yPercent: 50,
         scrollTrigger: {
-            trigger: '.top-bar__logo',
-            markers: true,
+            trigger: '.header-bar__logo',
+            markers: false,
             start: 'top 100px',
             scrub: scrub
         }
     });
 
-    gsap.to('.top-bar__logo > span > span.slow2', {
+    gsap.to('.header-bar__logo > span > span.slow2', {
         yPercent: 60,
         scrollTrigger: {
-            trigger: '.top-bar__logo',
-            markers: true,
+            trigger: '.header-bar__logo',
+            markers: false,
             start: 'top 100px',
             scrub: scrub
         }
     });
 
-    gsap.to('.top-bar__logo > span > span.slow3', {
+    gsap.to('.header-bar__logo > span > span.slow3', {
         yPercent: 70,
         scrollTrigger: {
-            trigger: '.top-bar__logo',
-            markers: true,
+            trigger: '.header-bar__logo',
+            markers: false,
             start: 'top 100px',
             scrub: scrub
         }
     });
 }
 
-TopBar__init();
+
+HeaderBar__init();
+
+
+//스와이퍼
+function MySliderBox1__init() {
+    const swiper = new Swiper('.my-slider-box-1 .swiper-container', {
+        autoplay: {
+            delay: 5000, /* 5초 */
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '.my-slider-box-1__nav-btn-right',
+            prevEl: '.my-slider-box-1__nav-btn-left',
+        },
+        pagination: {
+            el: ".my-slider-box-1 .swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                const no = index + 1;
+                const url = 'https://picsum.photos/id/' + no + '/100/100';
+                return '<div class="' + className + '"><img class="block" src="' + url + '"></div>';
+                let img = "";
+                if (index == 0) {
+                    img = '<img src ="https://picsum.photos/id/1/64/64">';
+                }
+                else if (index == 1) {
+                    img = '<img src ="https://picsum.photos/id/2/64/64">';
+                }
+                else if (index == 2) {
+                    img = '<img src ="https://picsum.photos/id/3/64/64">';
+                }
+                else if (index == 3) {
+                    img = '<img src ="https://picsum.photos/id/4/64/64">';
+                }
+                else if (index == 4) {
+                    img = '<img src ="https://picsum.photos/id/5/64/64">';
+                }
+                else if (index == 5) {
+                    img = '<img src ="https://picsum.photos/id/6/64/64">';
+                }
+                else if (index == 6) {
+                    img = '<img src ="https://picsum.photos/id/7/64/64">';
+                }
+
+                return '<span class="' + className + ' ">' + img + '</span>';
+            },
+        },
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true
+    });
+}
+
+MySliderBox1__init();
+
+
+//슬라이더
+function SectionSlider__init(no) {
+    var mySwiper = new Swiper('.section-slider-' + no + ' .swiper-container', {
+        autoplay: {
+            delay: 5000,
+        },
+        pagination: {
+            el: '.section-slider-' + no + ' .swiper-pagination',
+            clickable: true
+        },
+    });
+}
+SectionSlider__init(1);
